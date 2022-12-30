e3b36ad6fd99801bfff19fbc7a54bc0d6bceaffe
Timmy Susai <timmytusharrajans@gmail.com>
2022-12-30 02:18:03 -0500

    Added a video that plays when the button is pressed

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,19 +1,32 @@
 package com.example.helloworld
 
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
+import android.view.View
 import android.widget.Button
+import android.widget.MediaController
 import android.widget.Toast
+import android.widget.VideoView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val videoView = findViewById<View>(R.id.video_view) as VideoView
+        videoView.setVideoURI(Uri.parse("android.resource://" + packageName + "/" + R.raw.sana_say_hello))
+        val mediaController = MediaController(this)
+        videoView.setMediaController(mediaController)
         val button = findViewById<Button>(R.id.helloButton)
         button.setOnClickListener {
             Log.v("Hello world", "Button clicked")
-            Toast.makeText(this, "Annyeong!", Toast.LENGTH_SHORT).show()
+            videoView.start()
+            Toast.makeText(this, "Annyeong!", Toast.LENGTH_SHORT).apply {
+                setGravity(Gravity.CENTER, 0, 280)
+            }.show()
         }
     }
 }
